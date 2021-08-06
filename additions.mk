59ffdf62ee8153309438293fd53321eaa898be38
Al Sutton <al.sutton@alsutton.com>
2021-08-06 12:09:35 +0100

    Add non_public vendor makefile if it's present

--- additions.mk
+++ additions.mk
@@ -1,6 +1,9 @@
 # Generic overlays
 PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/snappautomotive/common/overlay
 
+# Add non-public overlays if they exist
+$(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
+
 # Snapp Maps
 PRODUCT_PACKAGES += osmdroid
 
