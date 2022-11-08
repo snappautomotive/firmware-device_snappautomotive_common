94e59b6125c492515ca24bfe68060dc98e518eaf
Al Sutton <al.sutton@alsutton.com>
2022-11-08 18:38:48 +0000

    Add flag to remove boot animation in case this is a Lineage build

--- additions.mk
+++ additions.mk
@@ -8,5 +8,6 @@ $(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk
 PRODUCT_PACKAGES += osmdroid
 
 # Boot Animation
+OVERRIDE_BOOT_ANIMATION=true
 PRODUCT_COPY_FILES += \
     device/snappautomotive/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
