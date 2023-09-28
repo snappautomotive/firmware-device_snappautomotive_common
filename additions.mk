9ae7b1cd41828954c7ccb77f507f951fe2b853cb
Al Sutton <al.sutton@alsutton.com>
2023-09-28 16:49:55 +0100

    Add AOSP template host

--- additions.mk
+++ additions.mk
@@ -5,8 +5,11 @@ PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/snappautomo
 $(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
 
 # Snapp Maps
-PRODUCT_PACKAGES += osmdroid
+PRODUCT_PACKAGES += \
+	osmdroid \
+	aosp-template-host
 
 # Boot Animation
+OVERRIDE_BOOT_ANIMATION=true
 PRODUCT_COPY_FILES += \
     device/snappautomotive/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
