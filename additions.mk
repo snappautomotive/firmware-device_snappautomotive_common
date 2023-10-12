a6b61733e1758c72c321461d47342b505c5f4f50
Al Sutton <al.sutton@alsutton.com>
2023-10-12 10:48:38 +0100

    Switch to using a declaration which is safe for inherit-product

--- additions.mk
+++ additions.mk
@@ -1,5 +1,5 @@
 # Generic overlays
-PRODUCT_PACKAGE_OVERLAYS += device/snappautomotive/common/overlay
+PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/common/overlay
 
 # Add non-public overlays if they exist
 $(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
