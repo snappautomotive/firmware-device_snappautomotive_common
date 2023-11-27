77244cb988a3daea2d97409eca3b618341908f12
Al Sutton <al.sutton@alsutton.com>
2023-11-27 09:54:38 +0000

    Add car specific settings to ensure they get set

--- additions.mk
+++ additions.mk
@@ -1,3 +1,14 @@
+TARGET_USES_CAR_FUTURE_FEATURES := true
+
+# CarServiceHelperService accesses the hidden api in the system server.
+SYSTEM_OPTIMIZE_JAVA := false
+
+# Include EVS reference implementations
+ENABLE_EVS_SAMPLE := true
+
+# Override heap growth limit due to high display density on device
+PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.heapgrowthlimit=256m
+
 # Generic overlays
 PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/snappautomotive/common/overlay
 
