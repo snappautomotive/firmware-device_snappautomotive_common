f7bafabba6459273bb2e5dece4d8a7134ea99001
Al Sutton <al.sutton@alsutton.com>
2024-06-10 08:57:29 +0100

    Allow for different build variants for the non_public repos

--- additions.mk
+++ additions.mk
@@ -1,8 +1,12 @@
 # Generic overlays
 PRODUCT_PACKAGE_OVERLAYS += device/snappautomotive/common/overlay
 
+ifndef BUILD_VARIANT
+  BUILD_VARIANT := snappautomotive
+endif
+
 # Add non-public overlays if they exist
-$(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
+$(call inherit-product-if-exists, vendor/$(BUILD_VARIANT)/non_public/additions.mk)
 
 # Snapp Provided Packages
 PRODUCT_PACKAGES += \
